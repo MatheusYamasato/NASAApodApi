53af42d2a249f5d6122f1e49a9e51378f0d0b3ec
MatheusYamasato <matheus-yamasato@hotmail.com>
2021-01-26 23:27:00 -0300

    finalizacao mvc e comecando logica botoes

--- index.js
+++ index.js
@@ -6,25 +6,20 @@ class Nasa {
         this._texto = "";
         this._data = "";
         this._imagem = "";
-    }
-
+    }    
     get texto() {
-
-    }
-
+        return this._texto;
+    }    
     get data() {
-        let data = new Date(this._data.value);
-    }
-
+        return this._data;
+    }    
     get imagem() {
-
-    }
-
+        return this._imagem;
+    }    
     buscarDados() {
         let xhr = new XMLHttpRequest();
-        const input = document.getElementById("data");
-        let API_KEY = "gP9xmpKhTuRPySvSsoB3rGNMsqkznLacF9Effufd";
-                
+        const API_KEY = "gP9xmpKhTuRPySvSsoB3rGNMsqkznLacF9Effufd";
+        let input = document.getElementById("data");        
         xhr.open("GET", `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${input.value}`, false);
         xhr.addEventListener("load" , () => {
             if(xhr.status == 200) {        
@@ -34,35 +29,39 @@ class Nasa {
                 this._imagem = imagem;
                 this._texto = texto;
                 this._data = input.value;
-                campoImagem.innerHTML = `<img class="imagem" src="${dadosAPI.url}">`;
-                //campoTexto.innerHTML = dadosAPI.explanation;
-            }
+                }
         })
         xhr.send();
-        
     }
 }
-
 class NasaController {
     buscarPessoa() {
+        // criamos uma nova "pessoa" da NASA para usamos os metodos que definimos na classe model
+        let dados = new Nasa();
+        dados.buscarDados();   
         
-        let info = new Nasa();
-        info.buscarDados();
-        
-        let visualizar = new NasaView(info);
-        visualizar.desenhar(campoImagem)
-    }
-
+        // chamamos a função do NasaView
+        let visualizar = new NasaView(dados);
+        visualizar.desenhar();
+    }   
 }
-
 // Cuida da vizualização
 class NasaView {
-    constructor(info)  {
-        this._elemento 
+
+    constructor(dados) {
+        this._texto = document.getElementById("texto");
+        this._imagem = document.getElementById("imagem");
+
+
+        // detalha o conteúdo dos elementos
+        this._imagem.src = dados.imagem;
+        this._texto.textContent = dados.texto;
     }
 
-    desenhar(elementoPai) {
-        elementoPai.append(this._elemento)
+    // Determina aonde vai ser inserido os campos
+    desenhar() {
+        campoImagem.append(this._imagem);
+        campoTexto.append(this._texto);
     }
 }
 
@@ -72,3 +71,9 @@ const btn = document.getElementById("procurarImagem");
 btn.addEventListener("click", () => {
     controller.buscarPessoa();
 })
+
+
+// 1ª Dar split no value da data, (-), retorna 3 posições AAAAMMDD
+
+// ---------------- Mais fácil ------------------------
+// 2ª Transformar o value da data num new Date("string")  
